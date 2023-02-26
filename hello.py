a3ecc9c3795705f7377ef42a41cf7bead24f9e11
Vũ Hoàng Long <44388919+longcon123@users.noreply.github.com>
2023-02-27 04:51:17 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello!")
+def count_substring(s, sub_string):
+  #viết chương trình đếm số lần xuất hiện của sub_string so với chuỗi s
+  return 
+
+if __name__ == '__main__':
+  s = 'ABCDCDCE'
+  sub_string = 'CDC'
+  count = count_substring(s, sub_string)
+  if count == 2:
+    print("Ch
+  print(count)
